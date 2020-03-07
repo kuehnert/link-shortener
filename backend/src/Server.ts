f26a7c2f437dd0a5b6d46483309588d5ce733598
Matthias Kuehnert <matthias.kuehnert@marienschule.com>
2020-03-07 15:07:41 +0100

    add backend tsed passport template

---
+++ backend/src/Server.ts
@@ -0,0 +1,76 @@
+import "@tsed/ajv";
+import {GlobalAcceptMimesMiddleware, ServerLoader, ServerSettings} from "@tsed/common";
+import "@tsed/passport";
+import "@tsed/swagger";
+import * as bodyParser from "body-parser";
+import * as compress from "compression";
+import * as cookieParser from "cookie-parser";
+import * as cors from "cors";
+import * as session from "express-session";
+import * as methodOverride from "method-override";
+import {CalendarCtrl} from "./controllers/calendars/CalendarCtrl";
+import {PassportCtrl} from "./controllers/passport/PassportCtrl";
+import {User} from "./models/User";
+
+const rootDir = __dirname;
+
+@ServerSettings({
+  rootDir,
+  acceptMimes: ["application/json"],
+  logger: {
+    debug: false,
+    logRequest: true,
+    requestFields: ["reqId", "method", "url", "query", "params", "duration"]
+  },
+  componentsScan: [
+    `${rootDir}/services/**/*.ts`,
+    `${rootDir}/protocols/**/*.ts`
+  ],
+  mount: {
+    "/rest": [
+      CalendarCtrl,
+      PassportCtrl
+    ]
+  },
+  swagger: {
+    path: "/api-docs"
+  },
+  calendar: {
+    token: true
+  },
+  passport: {
+    userInfoModel: User
+  }
+})
+export class Server extends ServerLoader {
+  /**
+   * This method let you configure the middleware required by your application to works.
+   * @returns {Server}
+   */
+  $beforeRoutesInit(): void | Promise<any> {
+    this
+      .use(GlobalAcceptMimesMiddleware)
+      .use(cors())
+      .use(cookieParser())
+      .use(compress({}))
+      .use(methodOverride())
+      .use(bodyParser.json())
+      .use(bodyParser.urlencoded({
+        extended: true
+      }))
+      .use(session({
+        secret: "mysecretkey",
+        resave: true,
+        saveUninitialized: true,
+        // maxAge: 36000,
+        cookie: {
+          path: "/",
+          httpOnly: true,
+          secure: false,
+          maxAge: null
+        }
+      }));
+
+    return null;
+  }
+}
